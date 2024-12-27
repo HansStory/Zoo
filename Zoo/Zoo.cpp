b19eccac8c6c51aebfd47eed17717d9f4ba04681
Han Stroy <gkswjdgur552@gmail.com>
2024-12-27 12:30:02 +0900

    [2번 과제] 필수기능

--- Zoo/Zoo.cpp
+++ Zoo/Zoo.cpp
@@ -1,20 +1,51 @@
-// Zoo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
 
-#include <iostream>
+class Animal {
+public :
+	Animal()
+	{
+
+	}
+
+	~Animal()
+	{
+
+	}
+
+	virtual void makeSound() const = 0;
+};
+
+class Dog : public Animal {
+public:
+	void makeSound() const override {
+		std::cout << "월월!!" << std::endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void makeSound() const override {
+		std::cout << "야옹~" << std::endl;
+	}
+};
+
+class Cow : public Animal {
+public:
+	void makeSound() const override {
+		std::cout << "음머~~~~" << std::endl;
+	}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Animal* pAnimalArr[3];
+
+	pAnimalArr[0] = new Dog();
+	pAnimalArr[1] = new Cat();
+	pAnimalArr[2] = new Cow();
+
+	for (int i = 0; i < 3; ++i) {
+		pAnimalArr[i]->makeSound();
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
